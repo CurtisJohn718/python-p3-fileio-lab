bb55310571cb25eb3123397efe9ba21e35057c01
Curtis <curtisjohn718@aol.com>
2025-04-10 12:29:34 -0400

    completed file io lab

--- lib/file_io.py
+++ lib/file_io.py
@@ -1,8 +1,19 @@
+# write
 def write_file(file_name, file_content):
-    pass
+    file_name = file_name.with_suffix(".txt")
+    with open(file_name, mode="w", encoding="utf-8") as file:
+        file.write(file_content)
 
+
+# append
 def append_file(file_name, append_content):
-    pass
+    file_name = file_name.with_suffix(".txt")
+    with open(file_name, mode="a", encoding="utf-8") as file:
+        file.write(append_content)
 
+
+# read
 def read_file(file_name):
-    pass
+    file_name = file_name.with_suffix(".txt")
+    with open(file_name, mode="r", encoding="utf-8") as file:
+        return file.read()
